0d8149e0405204d25d201de7d47eea4e88914ca8
Samtfm <smfemail@gmail.com>
2020-04-13 12:16:44 -0500

    Updates

---
+++ precache-manifest.e50643e23f36f667bee48f6ce81c88ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59eae40847b3e5551b97795779b02b2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7057cd214d5744d23144",
+    "url": "/static/css/main.1b72e7d3.chunk.css"
+  },
+  {
+    "revision": "bd92b2ed3a95f2d2c459",
+    "url": "/static/js/2.f2f76e34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f2f76e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7057cd214d5744d23144",
+    "url": "/static/js/main.0d993887.chunk.js"
+  },
+  {
+    "revision": "a5310d19d1e7c9f36d6d",
+    "url": "/static/js/runtime-main.81e96898.js"
+  }
+]);
